2f69dc74484973573c0e80b5387b767c428810cb
BenGitHubFlinders <69139088+BenGitHubFlinders@users.noreply.github.com>
2021-10-10 20:47:17 +1030

    Update sidemenu.php

--- New Project/components/sidemenu.php
+++ New Project/components/sidemenu.php
@@ -4,9 +4,11 @@
         <a class="sidemenu-btn" >Accounting</a>
         <div>
             <a class="sidemenu-subbtn" href="/CreateNewProject.php">Create New Project</a>
-            <a class="sidemenu-subbtn" href="/CreateNewProject.php">Manage Accounts</a>
-            <a class="sidemenu-subbtn" href="/CreateNewProject.php">Manage Users</a>
+            <a class="sidemenu-subbtn" href="/ManageAccount.php">Manage Accounts</a>
+            <a class="sidemenu-subbtn" href="/ManageUsers.php">Manage Users</a>
             <a class="sidemenu-subbtn" href="/ManageProject.php">Manage Projects</a>
+            <a class="sidemenu-subbtn" href="/ManageAccount.php">Manage Accounts</a>
+
           
 
 
@@ -15,7 +17,8 @@
 
     <a class="sidemenu-btn" >Inventory</a>
     <div>
-    <a class = "sidemenu-subbtn" href ="/categories.php">Manage Item Categories</a>
+    <a class = "sidemenu-subbtn" href ="/ManageItemCategories.php">Manage Item Categories</a>
+    <a class = "sidemenu-subbtn" href ="/ManageSuppliers.php">Manage Suppliers</a>
     </div>
 
     
