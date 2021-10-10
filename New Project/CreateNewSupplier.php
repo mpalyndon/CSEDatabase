87fd85e5146105d92296c766493d00d7cf888723
BenGitHubFlinders <69139088+BenGitHubFlinders@users.noreply.github.com>
2021-10-10 20:53:08 +1030

    Add files via upload

---
+++ New Project/CreateNewSupplier.php
@@ -0,0 +1,67 @@
+<?php require_once "components/layout_top.php"; ?>
+
+<div>
+
+<h1 class="subheader">CSE Store | Manage Suppliers | Create New Supplier</h1>
+
+<h2>
+        
+
+          <label> Name</label>
+          <br>
+          <input placeholder="">
+            <br>
+            <label> Address</label>
+            <br>
+            <input placeholder="">
+            <br>
+            <label> Contact Name</label>
+            <br>
+            <input placeholder= "">
+            <br>
+            <label> Phone Number</label>
+            <br>
+            <input placeholder="">
+            <br>
+            <label> Mobile Number</label>
+            <br>
+            <input placeholder="">
+            <br>
+            <label> Fax Number</label>
+            <br>
+            <input placeholder= "">
+            <br>
+            <label> Web Address</label>
+            <br>
+            <input placeholder = "">
+            <br>
+            <label> Email Address</label>
+            <br>
+            <input placeholder = "">
+            <br>
+            <label>Notes</label>
+            <br>
+            <input placeholder ="">
+            <br>
+            <br>
+            <br>
+            
+
+            
+            <!--<input type="text" name="name" placeholder="Required" required minlength="2" maxlength="more"> -->
+            <input type="Add" value="Add" class="primary-button">
+            <input type="Cancel" value="Cancel" class="primary-button">
+
+            
+          </h2>  
+            </div>
+        
+
+
+
+</form>
+</div>
+
+<?php require_once "components/layout_bottom.php"; ?>
+
+
