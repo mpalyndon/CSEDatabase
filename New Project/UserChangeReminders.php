dd5b628867b0d63bad54fea2d5a3ff3a1dbd0c5e
BenGitHubFlinders <69139088+BenGitHubFlinders@users.noreply.github.com>
2021-10-19 15:07:17 +1030

    Add files via upload

---
+++ New Project/UserChangeReminders.php
@@ -0,0 +1,52 @@
+<?php require_once "components/layout_top.php"; ?>
+
+<div>
+    <h1 class="subheader">CSE | User Change Reminders</h1>
+    <br>
+    <a class="primary-button" href="/index.php">Back to Store</a></h2>
+    <br>
+    <br>
+    <TABLE BORDER="3"    WIDTH="100%"   CELLPADDING="1" CELLSPACING="1">
+   <TR>
+      <TH COLSPAN="13"><BR><h1><b>Users with Reminders</b></h1>
+      </TH>
+   </TR>
+   <TR>
+      <TH><p></p></TH>
+      <TH><p>First Name</p></TH>
+      <TH><p>surName</p></TH>
+      <TH><p>Preferred Name</p></TH>
+      <TH><p>Username</p></TH>
+      <TH><p>Barcodes</p></TH>
+      <TH><p>Location</p></TH>
+      <TH><p>Type</p></TH>
+      <TH><p>Start Date</p></TH>
+      <TH><p>Finish Date</p></TH>
+      <TH><p>School Area</p></TH>
+      <TH><p>Change Reminder</p></TH>
+      <TH><p>Reminder Notes</p></TH>
+   </TR>
+   <TR ALIGN="CENTER">
+   <TD><a class="primary-button" >Edit</a> </TD>
+      <TD><p>Data 2</p></TD>
+      <TD><p>Data 3</p></TD>
+      <TD><p>Data 4</p></TD>
+      <TD><p>Data 5</p></TD>
+      <TD><p>Data 6</p></TD>
+      <TD><p>Data 7</p></TD>
+      <TD><p>Data 8</p></TD>
+      <TD><p>Data 9</p></TD>
+      <TD><p>Data 10</p></TD>
+      <TD><p>Data 11</p></TD>
+      <TD><p>Data 12</p></TD>
+      <TD><p>Data 13</p></TD>
+   </TR>
+</TABLE>
+
+
+        
+
+    </form>
+</div>
+
+    <?php require_once "components/layout_bottom.php"; ?>
