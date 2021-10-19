fd19874c5baa52557529437946b2b45ce58d53b8
BenGitHubFlinders <69139088+BenGitHubFlinders@users.noreply.github.com>
2021-10-19 16:16:06 +1030

    Update ManageUsers.php

--- New Project/ManageUsers.php
+++ New Project/ManageUsers.php
@@ -28,7 +28,7 @@
       <TH><p>Manager</p></TH>
    </TR>
    <TR ALIGN="CENTER">
-       <TD><a class="primary-button" >Edit</a> </TD>
+       <TD><a class="primary-button" >Edit</a> <a class="primary-button" >Delete</a> </TD>
       <TD><p>Data 2</p></TD>
       <TD><p>Data 3</p></TD>
       <TD><p>Data 4</p></TD>
